b943ae6f9a342ccdf0fe26f505d26e0a05a8949a
Simon <simon.dinis@univ-rouen.fr>
2025-11-05 11:29:48 +0100

    ajout de get-artifacts1.sh

---
+++ TP-Graph/get-artifacts1.sh
@@ -0,0 +1,13 @@
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: $(cat ../token.gh)" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/LarryEtte/graphes/actions/artifacts > gh-artifacts.json
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: $(cat ../token.gh)" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/LarryEtte/graphes/actions/artifacts/4472443985/zip \
+  --output artifact.zip
+
+unzip artifact
